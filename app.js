3879f3070c9fef906ea0de3eb8efe218d51a5cc3
Hashiq V H <mohamedhashiq88@gmail.com>
2024-05-03 22:56:16 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const Razorpay = require("razorpay");
+const WebSocket = require("ws");
+
+const app = express();
+const PORT = process.env.PORT || "3000";
+
+// Initialize Razorpay instance with your API key and secret
+const razorpayInstance = new Razorpay({
+  key_id: "rzp_test_fiIwmRET6CApc2", // Replace with your key_id
+  key_secret: "YAEUthsup8SijNs3iveeVlL1", // Replace with your key_secret
+});
+
+// Middleware to parse JSON request body
+app.use(bodyParser.json());
+
+// Create a WebSocket server
+const wss = new WebSocket.Server({ port: 8080 }); // Use any available port
+
+// WebSocket connection handler
+wss.on("connection", (ws) => {
+  console.log("WebSocket client connected");
+});
+
+// Route to handle incoming webhook requests from Razorpay
+app.post("/webhook", (req, res) => {
+  const webhookSignature = req.get("x-razorpay-signature");
+  const { body } = req;
+
+  try {
+    // Verify the webhook signature
+    const isValidSignature = razorpayInstance.validateWebhookSignature(
+      body,
+      webhookSignature,
+      "your_webhook_secret"
+    ); // Replace 'your_webhook_secret' with your actual webhook secret
+    if (!isValidSignature) {
+      console.error("Invalid webhook signature");
+      return res.status(400).end();
+    }
+
+    // Process the webhook event
+    console.log("Webhook event received:", body);
+    // Handle the webhook event according to your requirements
+
+    // Send the event to connected WebSocket clients
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(body));
+      }
+    });
+
+    // Send response to Razorpay confirming receipt of webhook
+    res.status(200).end();
+  } catch (error) {
+    console.error("Error processing webhook:", error);
+    res.status(500).end();
+  }
+});
+
+app.listen(PORT, () => {
+  console.log("Server is Listening on Port ", PORT);
+});
