cc71501a7d0a2f109f27dcb055ef759f26b1ec88
Hashiq V H <mohamedhashiq88@gmail.com>
2024-05-21 14:54:15 +0400

    issue

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const Razorpay = require("razorpay");
 const WebSocket = require("ws");
-const crypto = require("crypto");
 
 const app = express();
 const PORT = process.env.PORT || "3000";
@@ -23,7 +22,7 @@ const wss = new WebSocket.Server({ noServer: true });
 wss.on("connection", (ws) => {
   console.log("WebSocket client connected");
 
-  // You can handle messages received from clients here
+  // Handle messages received from clients
   ws.on("message", (message) => {
     console.log("Received message from client:", message);
   });
@@ -31,7 +30,7 @@ wss.on("connection", (ws) => {
 
 // Attach WebSocket server to the HTTP server created by Express
 const server = app.listen(PORT, () => {
-  console.log("Server is Listening on Port ", PORT);
+  console.log("Server is Listening on Port", PORT);
 });
 
 // Upgrade HTTP server to WebSocket server for handling WebSocket connections
@@ -40,8 +39,10 @@ server.on("upgrade", (request, socket, head) => {
     wss.emit("connection", ws, request);
   });
 });
+
 const processedEventIds = new Set();
 
+// Middleware to check for duplicate events
 const checkDuplicateEvent = (req, res, next) => {
   const eventId = req.headers["x-razorpay-event-id"];
 
@@ -55,48 +56,35 @@ const checkDuplicateEvent = (req, res, next) => {
     next();
   }
 };
+
 // Route to handle incoming webhook requests from Razorpay
 app.post("/webhook", checkDuplicateEvent, async (req, res) => {
   try {
-    let secret_key = "qwerasdfzxcv321";
-    // do a validation
+    let body = req.body;
+    // Extract required fields from the incoming JSON
+    const { event, payload } = body;
+    const { qr_code } = payload;
+    const { id, customer_id } = qr_code.entity;
 
-    const data = crypto.createHmac("sha256", secret_key);
+    // Construct a new object with required fields
+    const newData = {
+      event: event,
+      customer_id: customer_id,
+      id: id,
+      event_id: req.headers["x-razorpay-event-id"],
+    };
 
-    data.update(JSON.stringify(req.body));
+    // Convert the new object to a JSON string
+    const jsonString = JSON.stringify(newData);
 
-    const digest = data.digest("hex");
+    // Send the JSON string to all connected WebSocket clients
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(jsonString);
+      }
+    });
 
-    if (digest === req.headers["x-razorpay-signature"]) {
-      console.log("Valid Razorpay webhook received");
-    } else {
-      let body = req.body;
-      // Extract required fields from the incoming JSON
-      const { event, payload } = body;
-      const { qr_code } = payload;
-      const { id, customer_id } = qr_code.entity;
-
-      // Construct a new object with required fields
-      const newData = {
-        event: event,
-        customer_id: customer_id,
-        id: id,
-        event_id: req.headers["x-razorpay-event-id"],
-      };
-
-      // Convert the new object to a JSON string
-      const jsonString = JSON.stringify(newData);
-
-      // Send the JSON string to all connected WebSocket clients
-      wss.clients.forEach((client) => {
-        if (client.readyState === WebSocket.OPEN) {
-          client.send(jsonString);
-        }
-      });
-      const eventId = req.headers["x-razorpay-event-id"];
-      console.log(eventId);
-      console.log("Invalid Razorpay webhook received:", body);
-    }
+    console.log(`Processed event with ID ${req.headers["x-razorpay-event-id"]}`);
 
     // Send response to Razorpay confirming receipt of webhook
     res.status(200).end();
